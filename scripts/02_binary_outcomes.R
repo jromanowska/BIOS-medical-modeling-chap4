28ff1edccb5c323751384fd48e06789ad4d1f2a7
Julia Romanowska <Julia.Romanowska@uib.no>
2024-12-18 10:10:44 +0100

    presentation complete; more exploration of 'cath' data

--- scripts/02_binary_outcomes.R
+++ scripts/02_binary_outcomes.R
@@ -8,6 +8,7 @@ library(medicaldata)
 library(tidyverse)
 library(performance)
 library(rpart)
+library(rattle)
 library(caret)
 library(ranger) # for random forests
 library(naniar)
@@ -20,7 +21,14 @@ skimr::skim(cath)
 
 ggplot(cath, aes(age, choleste)) +
   geom_miss_point() +
-  facet_wrap(vars(sex)) +
+  facet_wrap(
+    vars(sex),
+    labeller = label_both
+  ) +
+  theme_bw()
+
+ggplot(cath, aes(sigdz, choleste)) +
+  geom_miss_point(position = position_jitter(width = 0.3)) +
   theme_bw()
 
 cath_fcts <- cath |>
@@ -29,16 +37,39 @@ cath_fcts <- cath |>
 GGally::ggpairs(cath_fcts)
 
 # try some trees ----
+cath_non_missing <- cath_fcts |>
+  filter(!is.na(choleste))
+cath_non_missing
+## create train-test split
+split_data <- createDataPartition(
+  cath_non_missing$sigdz,
+  p = 0.7,
+  list = FALSE
+)
+cath_train <- cath_non_missing[split_data, ]
+cath_test <- cath_non_missing[-split_data, ]
+
 (cath_tree1 <- rpart(
   formula = sigdz ~ .,
-  data = cath_fcts,
+  data = cath_train,
   method = "anova"
 ))
+fancyRpartPlot(cath_tree1)
+
 plotcp(cath_tree1)
 
-# cannot do this with missing values:
+## caret cross-validation
 (cath_tree2 <- caret::train(
   sigdz ~ .,
-  data = cath_fcts,
-  method = "rpart"
+  data = cath_train,
+  method = "rpart",
+  trControl = trainControl(method = "cv", number = 10),
+  tuneLength = 20
 ))
+ggplot(cath_tree2)
+
+## feature interpretation
+vip(
+  cath_tree2,
+  
+)
